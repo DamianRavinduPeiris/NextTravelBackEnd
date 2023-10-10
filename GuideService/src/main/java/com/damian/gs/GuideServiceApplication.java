e6a5011e992e2fde2ab0f7979b844ec514c0ad8a
Damian Peiris. <drpeiris3@gmail.com>
2023-10-10 21:43:10 +0530

    Re-added Guide Service module.

--- GuideService/src/main/java/com/damian/gs/GuideServiceApplication.java
+++ GuideService/src/main/java/com/damian/gs/GuideServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GuideServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GuideServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GuideServiceApplication.class, args);
+    }
 
 }
