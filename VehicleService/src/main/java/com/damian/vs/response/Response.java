8c07b2357faf20e04628dad3ddb4d6cd2eac9bbc
Damian Peiris. <drpeiris3@gmail.com>
2023-10-10 15:53:12 +0530

    Added an exception handler + response handler.

---
+++ VehicleService/src/main/java/com/damian/vs/response/Response.java
@@ -0,0 +1,15 @@
+package com.damian.vs.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Component
+public class Response {
+    private String message;
+    private Object data;
+}
---
+++ VehicleService/src/main/java/com/damian/vs/service/SuperService.java
@@ -0,0 +1,4 @@
+package com.damian.vs.service;
+
+public interface SuperService <T,ID>{
+}
