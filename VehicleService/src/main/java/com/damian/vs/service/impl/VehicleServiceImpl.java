bdec6b6e76be2e8d9eb2c5ec280ddc85f5232acd
Damian Peiris. <drpeiris3@gmail.com>
2023-10-10 16:04:24 +0530

    Completed service layer implementation of VS.

--- VehicleService/src/main/java/com/damian/vs/service/impl/VehicleServiceImpl.java
+++ VehicleService/src/main/java/com/damian/vs/service/impl/VehicleServiceImpl.java
@@ -1,14 +1,20 @@
 package com.damian.vs.service.impl;
 
 import com.damian.vs.dto.VehicleDTO;
+import com.damian.vs.entity.Vehicle;
 import com.damian.vs.repo.VehicleRepo;
 import com.damian.vs.response.Response;
 import com.damian.vs.service.custom.VehicleService;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -18,34 +24,71 @@ public class VehicleServiceImpl implements VehicleService {
     @Autowired
     private Response response;
     @Autowired
-    private ModelMapper Mapper;
+    private ModelMapper mapper;
+
     @Override
     public ResponseEntity<Response> add(VehicleDTO vehicleDTO) {
-        return null;
+        if (search(vehicleDTO.getVehicleId()).getBody().getData() == null) {
+            vehicleRepo.save(mapper.map(vehicleDTO, Vehicle.class));
+            return createAndSendResponse(HttpStatus.CREATED.value(), "Vehicle successfully saved!", null);
+
+        }
+        throw new RuntimeException("Vehicle already exists!");
+
+
     }
 
     @Override
     public ResponseEntity<Response> update(VehicleDTO vehicleDTO) {
-        return null;
+        if (search(vehicleDTO.getVehicleId()).getBody().getData() == null) {
+            throw new RuntimeException("Vehicle does not exist!");
+
+        }
+        vehicleRepo.save(mapper.map(vehicleDTO, Vehicle.class));
+        return createAndSendResponse(HttpStatus.OK.value(), "Vehicle successfully updated!", null);
     }
 
     @Override
     public ResponseEntity<Response> search(String s) {
-        return null;
+        Optional<Vehicle> vehcicle = vehicleRepo.findById(s);
+        if (vehcicle.isPresent()) {
+            return createAndSendResponse(HttpStatus.FOUND.value(), "Vehicle successfully retrieved!", mapper.map(vehcicle.get(), VehicleDTO.class));
+
+        }
+        return createAndSendResponse(HttpStatus.NOT_FOUND.value(), "Vehicle not found", null);
     }
 
     @Override
     public ResponseEntity<Response> delete(String s) {
-        return null;
+        if (search(s).getBody().getData() == null) {
+            throw new RuntimeException("Vehicle does not exist!");
+
+        }
+        vehicleRepo.deleteById(s);
+        return createAndSendResponse(HttpStatus.OK.value(), "Vehicle successfully deleted!", null);
+
     }
 
     @Override
     public ResponseEntity<Response> getAll() {
-        return null;
+
+        List<Vehicle> vehicles = vehicleRepo.findAll();
+        if (vehicles.isEmpty()) {
+            throw new RuntimeException("No vehicles found!");
+
+        }
+        List<VehicleDTO> vehicleDTOS = new ArrayList<>();
+        vehicles.forEach((vehicle) -> {
+            vehicleDTOS.add(mapper.map(vehicle, VehicleDTO.class));
+
+        });
+        return createAndSendResponse(HttpStatus.FOUND.value(), "Vehicles successfully retrieved!", vehicleDTOS);
     }
 
     @Override
     public ResponseEntity<Response> createAndSendResponse(int statusCode, String msg, Object data) {
-        return null;
+        response.setMessage(msg);
+        response.setData(data);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(statusCode));
     }
 }
