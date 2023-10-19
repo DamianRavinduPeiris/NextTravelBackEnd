a3f2f3e5ec296a1728122f386724384991674534
Damian Peiris. <drpeiris3@gmail.com>
2023-10-19 22:48:20 +0530

    Added PackageDetails Service.

---
+++ PackageDetailsService/src/main/java/com/damian/pds/PackageDetailsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.damian.pds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageDetailsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackageDetailsServiceApplication.class, args);
+    }
+
+}
