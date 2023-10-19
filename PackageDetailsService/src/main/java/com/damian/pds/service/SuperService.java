4d2478eb560db36031a5e83fb090ee4327553e50
Damian Peiris. <drpeiris3@gmail.com>
2023-10-19 22:52:39 +0530

    Added an exception handler + response entity for the PDS.

---
+++ PackageDetailsService/src/main/java/com/damian/pds/service/SuperService.java
@@ -0,0 +1,14 @@
+package com.damian.pds.service;
+
+import com.damian.pds.response.Response;
+import org.springframework.http.ResponseEntity;
+
+public interface SuperService <T,ID>{
+    ResponseEntity<Response> add(T t);
+    ResponseEntity<Response>update(T t);
+    ResponseEntity<Response>search(ID id);
+    ResponseEntity<Response>delete(ID id);
+    ResponseEntity<Response>getAll();
+    ResponseEntity<Response>createAndSendResponse(int statusCode,String msg,Object data);
+
+}
