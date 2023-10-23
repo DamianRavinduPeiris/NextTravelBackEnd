5b5112762b241b24efb011925b967dba559745e5
Damian Peiris. <drpeiris3@gmail.com>
2023-10-23 22:47:23 +0530

    Added an email server.

---
+++ EmaillService/src/main/java/com/damian/es/EmaillServiceApplication.java
@@ -0,0 +1,13 @@
+package com.damian.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmaillServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmaillServiceApplication.class, args);
+    }
+
+}
---
+++ EmaillService/src/test/java/com/damian/es/EmaillServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damian.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmaillServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
