ab94f1338196ee44ead7c9e03ed1e51152ec6371
Damian Peiris. <drpeiris3@gmail.com>
2023-10-25 22:34:21 +0530

    Finalized the service implementation of PDS + Secured PDS.

--- PackageDetailsService/src/main/java/com/damian/pds/service/impl/PackageDetailsServiceImpl.java
+++ PackageDetailsService/src/main/java/com/damian/pds/service/impl/PackageDetailsServiceImpl.java
@@ -1,15 +1,21 @@
 package com.damian.pds.service.impl;
 
 import com.damian.pds.dto.PackageDetailsDTO;
+import com.damian.pds.entity.PackageDetails;
 import com.damian.pds.repo.PackageDetailsRepo;
 import com.damian.pds.response.Response;
 import com.damian.pds.service.custom.PackageDetailsService;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -23,27 +29,65 @@ public class PackageDetailsServiceImpl implements PackageDetailsService {
 
     @Override
     public ResponseEntity<Response> add(PackageDetailsDTO packageDetailsDTO) {
-        return null;
+        if(search(packageDetailsDTO.getPackageDetailsId()).getBody().getData() == null){
+            packageDetailsRepo.save(mapper.map(packageDetailsDTO, PackageDetails.class));
+            return createAndSendResponse(HttpStatus.CREATED.value(), "PackageDetails Created Successfully!",null);
+
+        }
+        return createAndSendResponse(HttpStatus.CONFLICT.value(), "PackageDetails Already Exists!",null);
+
     }
 
     @Override
     public ResponseEntity<Response> update(PackageDetailsDTO packageDetailsDTO) {
-        return null;
+
+        if(search(packageDetailsDTO.getPackageDetailsId()).getBody().getData() != null){
+            packageDetailsRepo.save(mapper.map(packageDetailsDTO, PackageDetails.class));
+            return createAndSendResponse(HttpStatus.OK.value(), "PackageDetails Updated Successfully!",null);
+
+        }
+        return createAndSendResponse(HttpStatus.NOT_FOUND.value(), "PackageDetails Not Found!",null);
     }
 
     @Override
     public ResponseEntity<Response> search(String s) {
-        return null;
+        Optional<PackageDetails> pack = packageDetailsRepo.findById(s);
+        if(pack.isPresent()){
+            return createAndSendResponse(HttpStatus.FOUND.value(), "PackageDetails Retrieved successfully!",mapper.map(pack.get(),PackageDetailsDTO.class));
+
+        }
+        return createAndSendResponse(HttpStatus.NOT_FOUND.value(), "PackageDetails Not Found!",null);
+
+
     }
 
     @Override
     public ResponseEntity<Response> delete(String s) {
-        return null;
+        if(search(s).getBody().getData() == null){
+            return createAndSendResponse(HttpStatus.NOT_FOUND.value(), "PackageDetails Not Found!",null);
+
+        }
+        packageDetailsRepo.deleteById(s);
+        return createAndSendResponse(HttpStatus.OK.value(), "PackageDetails Deleted Successfully!",null);
+
+
     }
 
     @Override
     public ResponseEntity<Response> getAll() {
-        return null;
+        List<PackageDetails> packs = packageDetailsRepo.findAll();
+        if(packs.isEmpty()){
+            return createAndSendResponse(HttpStatus.NOT_FOUND.value(), "PackageDetails Not Found!",null);
+
+        }
+        List<PackageDetailsDTO> packageDetailsDTOList = new ArrayList<>();
+        packs.forEach((p)->{
+            packageDetailsDTOList.add(mapper.map(p,PackageDetailsDTO.class));
+
+        });
+        return createAndSendResponse(HttpStatus.FOUND.value(), "PackageDetails Retrieved Successfully!",packageDetailsDTOList);
+
+
     }
 
     @Override
