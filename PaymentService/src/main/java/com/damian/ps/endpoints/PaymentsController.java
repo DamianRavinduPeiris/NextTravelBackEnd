f0d52a46b9f9b409f007eef241a676de0147f6ff
Damian Peiris. <drpeiris3@gmail.com>
2023-10-25 22:20:45 +0530

    Exposed an endpoint for CRUD in payments + Secured PaymentService.

---
+++ PaymentService/src/main/java/com/damian/ps/endpoints/PaymentsController.java
@@ -0,0 +1,43 @@
+package com.damian.ps.endpoints;
+
+import com.damian.ps.dto.PaymentsDTO;
+import com.damian.ps.response.Response;
+import com.damian.ps.service.custom.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("")
+@CrossOrigin
+public class PaymentsController {
+    @Autowired
+    private PaymentService paymentService;
+    @PostMapping(path = "/savePayment",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Response>savePayment(@RequestBody PaymentsDTO paymentsDTO){
+        return paymentService.add(paymentsDTO);
+
+    }
+    @PutMapping(path = "/updatePayment",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Response>updatePayment(@RequestBody PaymentsDTO paymentsDTO){
+        return paymentService.update(paymentsDTO);
+
+    }
+    @DeleteMapping(path = "/deletePayment",params = "paymentID",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Response>deletePayment(@RequestParam("paymentID") String paymentID){
+        return paymentService.delete(paymentID);
+
+    }
+
+    @GetMapping(path = "/searchPayment",params = "paymentID",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Response>savePayment(@RequestParam("paymentID") String paymentID){
+        return paymentService.search(paymentID);
+
+    }
+    @GetMapping(path = "/getAllPayments",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Response>getAllPayments(){
+        return paymentService.getAll();
+
+    }
+}
