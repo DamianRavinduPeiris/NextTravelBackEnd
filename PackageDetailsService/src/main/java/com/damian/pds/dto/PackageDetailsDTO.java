4ee756341261a1553e8d16f5b208948d82ba08bc
Damian Peiris. <drpeiris3@gmail.com>
2023-10-28 12:44:50 +0530

    Added validation in PackageDetails DTO.

--- PackageDetailsService/src/main/java/com/damian/pds/dto/PackageDetailsDTO.java
+++ PackageDetailsService/src/main/java/com/damian/pds/dto/PackageDetailsDTO.java
@@ -2,6 +2,9 @@ package com.damian.pds.dto;
 
 import com.damian.pds.dto.superdto.SuperDTO;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,22 +18,42 @@ import java.util.Date;
 public class PackageDetailsDTO implements SuperDTO, Serializable {
     @Valid
     private String packageDetailsId;
+    @NotNull(message = "Package Id cannot be null.")
+    @NotBlank(message = "Package Id cannot be blank.")
     private String packageId;
+    @NotNull(message = "Package Category cannot be null.")
+    @NotBlank(message = "Package Category cannot be blank.")
     private String packageCategory;
+    @NotNull(message = "Hotel ID cannot be null.")
+    @NotBlank(message = "Hotel ID cannot be blank.")
     private String hotelId;
+    @NotNull(message = "Vehicle ID cannot be null.")
+    @NotBlank(message = "Vehicle ID cannot be blank.")
     private String vehicleId;
     private Date startDate;
     private Date endDate;
+    @Positive(message = "Value cannot be negative.")
     private int noOfDays;
+    @NotNull(message = "Travel Area cannot be null.")
+    @NotBlank(message = "Travel Area cannot be blank.")
     private String travelArea;
+    @Positive(message = "Value cannot be negative.")
     private int noOfAdults;
+    @Positive(message = "Value cannot be negative.")
     private int noOfChildren;
+    @Positive(message = "Value cannot be negative.")
     private int totalHeadCount;
     private  boolean petsStatus;
     private  boolean guideStatus;
+    @NotNull(message = "Guide ID cannot be null.")
+    @NotBlank(message = "Guide ID cannot be blank.")
     private String guideId;
+    @Positive(message = "Value cannot be negative.")
     private  double totalPackageValue;
+    @NotNull(message = "User ID cannot be null.")
+    @NotBlank(message = "User ID cannot be blank.")
     private  String userId;
+    @Positive
     private double paidValue;
     private String remarks;
 
